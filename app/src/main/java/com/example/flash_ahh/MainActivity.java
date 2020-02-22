bff390708751c3bd1c8c48503bf650125ab79382
rbenreuben <rbenreuben01@manhattan.edu>
2020-02-22 00:58:50 -0500

    Finished Lab #1 CodePath Android Studio

--- app/src/main/java/com/example/flash_ahh/MainActivity.java
+++ app/src/main/java/com/example/flash_ahh/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.flash_ahh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView question = findViewById(R.id.flashcard_question);
+        final TextView answer = findViewById(R.id.flashcard_answer);
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // show the answer
+                // hide the question
+                question.setVisibility(View.INVISIBLE);
+                answer.setVisibility(View.VISIBLE);
+
+                question.setBackgroundColor(getResources().getColor(R.color.RED));
+
+
+            }
+
+        });
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setVisibility(View.INVISIBLE);
+                question.setVisibility(View.VISIBLE);
+                answer.setBackgroundColor(getResources().getColor(R.color.BLUE));
+            }
+        });
     }
 }
